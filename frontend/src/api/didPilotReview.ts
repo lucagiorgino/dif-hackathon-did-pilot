14469e66d8fc4bf768192380cc71bb32702e3301
Matteo Midena <matteo.midena@protonmail.com>
2023-11-18 16:45:26 +0100

    add new reviews methods

--- frontend/src/api/didPilotReview.ts
+++ frontend/src/api/didPilotReview.ts
@@ -2,6 +2,7 @@ import { DidReview } from '@/types/types';
 import {
     Web5,
     Record,
+    RecordsQueryRequest,
 } from '@web5/api';
 import dwnConnector from './dwnConnector';
 
@@ -9,6 +10,18 @@ type ReviewResponse = {
     record: Record,
     review: DidReview
 }
+
+type ReviewsResponse = {
+    records: Record[] | undefined,
+    reviews: DidReview[]
+}
+
+type DidStats = {
+    totalReviews: number,
+    averageStars: number
+    reviewedSince: string,
+    totalReviewers: number
+  }
 
 
 const createReview = async (web5: Web5, review: DidReview) => {
@@ -85,11 +98,60 @@ const getReviewsByAuthor = async (web5: Web5, author: string) => {
         }
     })
 
-    // change parsedReviews to be an array of DidReview objects
-    const reviews = parsedReviews as DidReview[]
-
-    return { reviewRecords, reviews }
+    return { reviewRecords, reviews: parsedReviews as DidReview[] }
 }
 
-const didPilotReview = { createReview, getReviewsByAuthor, getReviewById, updateReviewById, deleteReviewById };
+const getReviewsByRecipient = async (web5: Web5, recipient: string): Promise<ReviewsResponse> => {
+    const { records, parsedRecords } = await dwnConnector.queryRecords(web5, {
+        message: {
+            filter: {
+                dataFormat: "application/json",
+                recipient: recipient,
+            }
+        }
+    })
+
+    return { records, reviews: parsedRecords as DidReview[] }
+}
+
+const getReviews = async (web5: Web5, query: RecordsQueryRequest): Promise<ReviewsResponse> => {
+    const { records, parsedRecords } = await dwnConnector.queryRecords(web5, query)
+
+    return { records, reviews: parsedRecords as DidReview[] }
+}
+
+const getDidStats = async (web5: Web5, did: string): Promise<DidStats> => {
+    const { records, reviews } = await getReviews(
+        web5,
+        {
+            message: {
+                filter: {
+                    dataFormat: "application/json",
+                    recipient: did,
+                },
+                // TODO: check if this is OKAY for us
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                dateSort: "createdDescending" as any
+            }
+        }
+    )
+    console.log("records", records)
+    console.log("reviews", reviews)
+    console.log("did", did)
+    const reviewers: string[] = []
+    records?.forEach(record => {
+        if (!reviewers.includes(record.author)) {
+        reviewers.push(record.author)
+        }
+    })
+
+    return {
+        totalReviews: reviews.length,
+        averageStars: reviews.reduce((acc, curr) => acc + curr.stars, 0) / reviews.length,
+        reviewedSince: records ? (new Date(records[0].dateCreated)).toLocaleDateString() : "NA",
+        totalReviewers: reviewers.length
+    }
+}
+
+const didPilotReview = { createReview, getReviewsByAuthor, getReviewById, updateReviewById, deleteReviewById, getReviewsByRecipient, getDidStats };
 export default didPilotReview;
